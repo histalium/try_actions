58585811a2df21ee6256f6c3656e41f0190676ef
histalium <histalium@outlook.com>
2019-11-24 18:28:23 +0000

    1 failing test

---
+++ TryActions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryActions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
